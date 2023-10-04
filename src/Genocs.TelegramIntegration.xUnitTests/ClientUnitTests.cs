ad12a8ff02248007bcaebeef69927ff975620f41
Nocco Giovanni Emanuele <giovanni.nocco@gmail.com>
2023-10-04 22:22:28 +0200

    fix unit test

--- src/Genocs.TelegramIntegration.xUnitTests/ClientUnitTests.cs
+++ src/Genocs.TelegramIntegration.xUnitTests/ClientUnitTests.cs
@@ -1,3 +1,6 @@
+using Genocs.Integration.CognitiveServices.Interfaces;
+using Genocs.Persistence.MongoDb.Repositories;
+using Genocs.TelegramIntegration.Domains;
 using Genocs.TelegramIntegration.Services;
 using Genocs.TelegramIntegration.Services.Interfaces;
 using Microsoft.Extensions.Logging;
@@ -11,24 +14,43 @@ public class ClientUnitTests
     [Fact]
     public void ConnectToClientTest()
     {
-        Options.TelegramSettings telegramOptions = new Options.TelegramSettings() { Token = "TelegramSettingOption" };
         var mockTelegramOptions = new Mock<IOptions<Options.TelegramSettings>>();
 
         // We need to set the Value of IOptions to be the Options.TelegramSettings Class
+        Options.TelegramSettings telegramOptions = new Options.TelegramSettings() { Token = "TelegramSettingOption" };
         mockTelegramOptions.Setup(ap => ap.Value).Returns(telegramOptions);
 
-        Options.ApiClientSettings apiClientOptions = new Options.ApiClientSettings() { FormRecognizerUrl = "TelegramSettingOption" };
+        var mockOpenAISettings = new Mock<IOptions<Options.OpenAISettings>>();
+
+        // We need to set the Value of IOptions to be the Options.OpenAISettings Class
+        Options.OpenAISettings openAIOptions = new Options.OpenAISettings() { APIKey = "APIKey", Url = "Url" };
+        mockOpenAISettings.Setup(ap => ap.Value).Returns(openAIOptions);
+
         var mockApiClientOptions = new Mock<IOptions<Options.ApiClientSettings>>();
 
         // We need to set the Value of IOptions to be the Options.ApiClientSettings Class
+        Options.ApiClientSettings apiClientOptions = new Options.ApiClientSettings() { FormRecognizerUrl = "TelegramSettingOption" };
         mockApiClientOptions.Setup(ap => ap.Value).Returns(apiClientOptions);
-
 
         // mock logger
         var mockLogger = new Mock<ILogger<TelegramProxy>>();
 
 
-        ITelegramProxy proxy = new TelegramProxy(mockTelegramOptions.Object, mockLogger.Object, null, mockApiClientOptions.Object, null, null);
+        var mockFormRecognizer = new Mock<IFormRecognizer>();
+        var mockImageClassifier = new Mock<IImageClassifier>();
+        var mockHttpClientFactory = new Mock<IHttpClientFactory>();
+
+        var mockMongoDbRepositoryGenocsChat = new Mock<IMongoDbRepository<GenocsChat>>();
+
+
+        ITelegramProxy proxy = new TelegramProxy(mockTelegramOptions.Object,
+                                                 mockLogger.Object,
+                                                 mockOpenAISettings.Object,
+                                                 mockApiClientOptions.Object,
+                                                 mockHttpClientFactory.Object,
+                                                 mockMongoDbRepositoryGenocsChat.Object,
+                                                 mockFormRecognizer.Object,
+                                                 mockImageClassifier.Object);
         proxy.PullUpdatesAsync();
 
     }
